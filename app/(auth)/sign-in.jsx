0f5d91e32f5e511d9db3a385483af535ee1b7f0b
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-10-22 09:53:58 +0200

    sign in screens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,14 +1,26 @@
 import { View, Text, ScrollView, Image } from "react-native";
-import React from "react";
+import React, {useState} from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import CustomButton from '../../components/CustomButton'
 import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import {Link} from 'expo-router'
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+
+  }
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
-        <View className="w-full justify-center h-full px4 my-6">
+        <View className="w-full justify-center min-h-[85vh] px4 my-6">
           <Image
             source={images.logo}
             resizeMode="contain"
@@ -17,6 +29,35 @@ const SignIn = () => {
           <Text className="text-2xl text-white font-psemibold mt-10">
             Log in to Prime
           </Text>
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+            
+          />
+
+          <CustomButton
+          title="Sign In"
+          handlePress={submit}
+          containerStyles='mt-7'
+          isLoading={isSubmitting}
+          /> 
+          <View className='justify-center pt-5 flex-row gap-2'>
+            <Text className='text-lg text-gray-100 font-pregular'>
+              Dont have an account?
+            </Text>
+            <Link href="/sign-up" className="text-lg font-psemibold text-secondary">Sign Up</Link>
+          </View>
+          
         </View>
       </ScrollView>
     </SafeAreaView>

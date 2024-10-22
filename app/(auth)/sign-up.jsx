8dc0548baf879fc65547e8673b3e15902904d8cc
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-10-22 14:50:11 +0200

    sign in finalized

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,22 +1,36 @@
-import { View, Text, ScrollView, Image } from "react-native";
-import React, {useState} from "react";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from '../../components/CustomButton'
+import CustomButton from "../../components/CustomButton";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
-import {Link} from 'expo-router'
+import { Link } from "expo-router";
+import { createUser } from "../../lib/appwrite";
+import { router } from "expo-router";
 
 const SignUp = () => {
   const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
-  const [isSubmitting, setIsSubmitting] = useState(false)
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.username === "" || !form.email === "" || !form.password === "") {
+      Alert.alert("Error", "Please fill in all fields");
+    }
+    setIsSubmitting(true);
+    try {
+      const result = await createUser(form.email, form.password, form.username);
 
-  }
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -30,42 +44,45 @@ const SignUp = () => {
           <Text className="text-2xl text-white font-psemibold mt-10">
             Sign up to Prime
           </Text>
+
           <FormField
-            title='Username'
-            value={form.email}
-            handleChangeText={(e) => setForm({...form, username: e})}
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
             otherStyles="mt-10"
-        
           />
           <FormField
-            title='Email'
+            title="Email"
             value={form.email}
-            handleChangeText={(e) => setForm({...form, email: e})}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
             otherStyles="mt-7"
             keyboardType="email-address"
           />
 
           <FormField
-            title='Password'
+            title="Password"
             value={form.password}
-            handleChangeText={(e) => setForm({...form, password: e})}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
             otherStyles="mt-7"
-            
           />
 
           <CustomButton
-          title="Sign Up"
-          handlePress={submit}
-          containerStyles='mt-7'
-          isLoading={isSubmitting}
-          /> 
-          <View className='justify-center pt-5 flex-row gap-2'>
-            <Text className='text-lg text-gray-100 font-pregular'>
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
               Have an account already?
             </Text>
-            <Link href="/sign-in" className="text-lg font-psemibold text-secondary">Sign in</Link>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign in
+            </Link>
           </View>
-          
         </View>
       </ScrollView>
     </SafeAreaView>
